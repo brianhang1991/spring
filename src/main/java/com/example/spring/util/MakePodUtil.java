dbe83fac8c90894cccbdbb9de523235bc382b9fd
hcy <hcy@ulang.com>
2020-07-29 10:19:28 +0800

    添加ehcache支持，完善ehcache配置

---
+++ src/main/java/com/example/spring/util/MakePodUtil.java
@@ -0,0 +1,34 @@
+package com.example.spring.util;
+
+import java.io.File;
+
+/**
+ * @program: spring
+ * @description: make pod util
+ * @author: hcy
+ * @create: 2020-07-29 09:47
+ **/
+public class MakePodUtil {
+
+    //        String SERVICE_FRONT : 0
+    //        SERVICE_BACK : 1
+    //        INTEGRATION_FRONT : 2
+    //        INTEGRATION_BACK : 3
+    public static void makePod(File file, String ip, String username,
+                               String password, String path, int podType){
+
+        switch (podType){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
+
+}
